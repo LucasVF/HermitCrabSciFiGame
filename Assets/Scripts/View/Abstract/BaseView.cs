60a9ae520c599cd585ad5e4c4a31ff53f208e4c8
LucasVF <lucas_v.f@hotmail.com>
2024-09-20 23:19:51 -0300

    update gitignore

--- Assets/Scripts/View/Abstract/BaseView.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-public abstract class BaseView : MonoBehaviour, IView {
-    public abstract void SetUpView();
-    public abstract ViewNames GetName();
-    public void DisplayView() => gameObject.SetActive(true);
-    public void HideView() => gameObject.SetActive(false);
-}

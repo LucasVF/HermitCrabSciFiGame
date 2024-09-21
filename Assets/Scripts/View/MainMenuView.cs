2df6f7a9035fe1e25f59009a4854ca9b18d6a0d2
LucasVF <lucas_v.f@hotmail.com>
2024-09-20 23:25:19 -0300

    Return project to Git

---
+++ Assets/Scripts/View/MainMenuView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuView : BaseView
+{
+    public override ViewNames GetName() => ViewNames.MainMenu;
+
+    public override void SetUpView()
+    {
+        
+    }
+}
